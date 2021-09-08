2c1d7f401228b871030bfdf14d2681f6310760b1
Deepak Meena <memarsahab@gmail.com>
2021-09-08 16:42:11 +0530

    default route setup

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,30 +1,34 @@
-const express = require('express')
+const express = require('express');
 
-const Property = require('../models/property')
-const geocoder = require('../utils/geocoder')
+const Property = require('../models/property');
+const geocoder = require('../utils/geocoder');
 
-const router=express.Router()
+const router = express.Router();
+
+//default route
+router.get('/', async (req, res) => {
+  return res.status(200).json({
+    success: true,
+  });
+});
 
 //make a room
 //on */rooms
 router.post('/room', async (req, res) => {
-
   try {
     const room = await Property.create(req.body);
     return res.status(201).json({
       success: true,
-      data: room
+      data: room,
     });
   } catch (err) {
     console.error(err);
     if (err.code === 11000) {
       return res.status(400).json({ error: 'This store already exists' });
     }
-    res.status(500).json({ error: 'Server error' })
+    res.status(500).json({ error: 'Server error' });
   }
-
 });
-
 
 //finde all the rooms
 //on */rooms
@@ -35,40 +39,35 @@ router.get('/rooms', async (req, res) => {
     return res.status(200).json({
       success: true,
       count: rooms.length,
-      data: rooms
+      data: rooms,
     });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
   }
-
 });
 
-
 //find rooms in redius of 50km where center is given location
-// */rooms/kota 
+// */rooms/kota
 router.get('/rooms/:loc', async (req, res) => {
-
-  const location= req.params.loc
+  const location = req.params.loc;
   const geoloc = await geocoder.geocode(location);
-  let coords = [geoloc[0].longitude, geoloc[0].latitude]
-        
+  let coords = [geoloc[0].longitude, geoloc[0].latitude];
+
   try {
-    const rooms = await Property.find({ location:
-      { $geoWithin:
-         { $centerSphere: [ coords, 50 / 6378 ] } } });
+    const rooms = await Property.find({
+      location: { $geoWithin: { $centerSphere: [coords, 50 / 6378] } },
+    });
 
     return res.status(200).json({
       success: true,
       count: rooms.length,
-      data: rooms
+      data: rooms,
     });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
   }
-
 });
 
-
-module.exports=router
+module.exports = router;
